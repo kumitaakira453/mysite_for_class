64af063cc18f09f9e33e4a67105b9eb6a3f4b1a7
kumitaakira453 <akira_kumita@demia.co.jp>
2024-08-24 19:40:19 +0900

    adminの編集

--- forum/admin.py
+++ forum/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Topic, Message
 
-admin.site.register(Topic)
+# admin.site.register(Topic)
 admin.site.register(Message)
+
+
+# インライン設定ができるよ
+class MessageInline(admin.StackedInline):
+    model = Message
+    extra = 2
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    inlines = [MessageInline]
